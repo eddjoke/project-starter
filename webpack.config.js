4c076b61207d11ef41ef5d0f61e7f722b1b9743e
Edvinas Jurele <edvinas.j@tesonet.lt>
2018-04-03 19:55:38 +0300

    Add babelrc, extend webpack config to support babelifying and scss styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].css',
+});
+
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
   module: {
     rules: [
       {
-        test: /\.css$/,
-        loader: 'css-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
       },
     ],
+  },
+  plugins: [extractSass],
+  devServer: {
+    contentBase: './dist',
   },
 };
